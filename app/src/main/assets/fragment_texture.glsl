dd12837b8014e301afe2f008401196b3eaddbe02
JackHou <jackhou1990@163.com>
2020-05-08 20:00:38 +0800

    更改原色着色器

--- app/src/main/assets/fragment_texture.glsl
+++ app/src/main/assets/fragment_texture.glsl
@@ -17,6 +17,7 @@ varying vec2 textureCoordinate;
 
 void main() {
     vec4 tc = texture2D(videoTex, textureCoordinate);
-    float color = tc.r * 0.3 + tc.g * 0.59 + tc.b * 0.11;//这里进行的颜色变换处理，传说中的黑白滤镜。
-    gl_FragColor = vec4(color,color,color,1.0);
+    //float color = tc.r * 0.3 + tc.g * 0.59 + tc.b * 0.11;//这里进行的颜色变换处理，传说中的黑白滤镜。
+    //gl_FragColor = vec4(color,color,color,1.0);
+    gl_FragColor = vec4(tc.r,tc.g,tc.b,1.0);//彩色
 }
